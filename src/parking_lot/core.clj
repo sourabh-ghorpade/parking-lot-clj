293fb3cbef384803bf8a7e4779d4a8dd8cef4b92
Sourabh Ghorpade <sourabh.g@go-jek.com>
2019-07-24 09:25:54 +0530

    Here be cars.

---
+++ src/parking_lot/core.clj
@@ -0,0 +1,6 @@
+(ns parking-lot.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/parking_lot/core_test.clj
@@ -0,0 +1,7 @@
+(ns parking-lot.core-test
+  (:require [clojure.test :refer :all]
+            [parking-lot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
