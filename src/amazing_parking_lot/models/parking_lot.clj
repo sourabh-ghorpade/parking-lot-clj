e94757b31369b7b208309c067f38a6a9e6328f13
Sourabh Ghorpade <sourabh.g@go-jek.com>
2019-09-22 12:27:42 +0530

    Add capability to park cars in a non-empty lot.

--- src/amazing_parking_lot/models/parking_lot.clj
+++ src/amazing_parking_lot/models/parking_lot.clj
@@ -2,8 +2,10 @@
 
 (defn create [number-of-slots]
   {:number-of-slots number-of-slots
-   :slots           [{} {} {}]})
+   :slots           (vec (repeat number-of-slots nil))})
 
 (defn park [car parking-lot]
-  {:message     "Parked in slot number 1"
-   :parking-lot (assoc parking-lot :slots [car])})
+  (let [free-slot-number (.indexOf (:slots parking-lot) nil)
+        slots-with-new-car (assoc (parking-lot :slots) free-slot-number car)]
+    {:message     (str "Parked in slot number " (+ free-slot-number 1))
+     :parking-lot (assoc parking-lot :slots slots-with-new-car)}))
--- test/amazing_parking_lot/models/parking_lot_test.clj
+++ test/amazing_parking_lot/models/parking_lot_test.clj
@@ -7,12 +7,23 @@
   (testing "creates a parking lot with the given slots"
     (let [value (create 3)]
       (is (= value {:number-of-slots 3
-                    :slots           [{} {} {}]})))))
+                    :slots           [nil nil nil]})))))
 
 (deftest park-test
-  (let [parking-lot (create 3)
-        car (car/create "ABC" "white")
-        result (park car parking-lot)]
-    (is (= (:message result) "Parked in slot number 1"))
-    (is (= (:parking-lot result) {:number-of-slots 3
-                                  :slots           [car]}))))
+  (testing "when the parking lot is empty"
+    (let [parking-lot (create 3)
+          car (car/create "ABC" "white")
+          result (park car parking-lot)]
+      (is (= (:message result) "Parked in slot number 1"))
+      (is (= (:parking-lot result) {:number-of-slots 3
+                                    :slots           [car nil nil]}))))
+
+  (testing "when the parking lot is not empty"
+    (let [car_one (car/create "ABC" "white")
+          car_two (car/create "PQR" "black")
+          parking-lot {:number-of-slots 3
+                       :slots           (vec [car_one nil nil])}
+          result (park car_two parking-lot)]
+      (is (= (:message result) "Parked in slot number 2"))
+      (is (= (:parking-lot result) {:number-of-slots 3
+                                    :slots           [car_one car_two nil]})))))
