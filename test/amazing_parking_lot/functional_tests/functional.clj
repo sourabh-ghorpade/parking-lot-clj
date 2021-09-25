924f6ce2eb981faa3510f84de323de607fb9e7b5
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2021-09-25 18:05:59 +0530

    Add functional test for parking lot not created.

--- test/amazing_parking_lot/functional_tests/functional.clj
+++ test/amazing_parking_lot/functional_tests/functional.clj
@@ -6,14 +6,22 @@
 (deftest functional-test
   (testing "it processes all inputs and generates an output till input is not the exit command"
     (let [output-capture-list (atom (vec []))
-          input-commands ["create_parking_lot 2"
+          input-commands ["park \"KA-123\" \"White\""
+                          "leave 1"
+                          "create_parking_lot 2"
                           "park \"KA-123\" \"White\""
                           "park \"KA-456\" \"Black\""
                           "park \"KA-789\" \"Red\""
                           "leave 1"
                           "park \"KA-789\" \"Red\""
                           "exit"]
-          expected-output ["Created parking lot with 2 slots" "Parked in slot number 1" "Parked in slot number 2"
-                           "Parking Lot is full" "Un-parked car \"KA-123\" at slot 1" "Parked in slot number 1"]]
+          expected-output ["Parking Lot not created"
+                           "Parking Lot not created"
+                           "Created parking lot with 2 slots"
+                           "Parked in slot number 1"
+                           "Parked in slot number 2"
+                           "Parking Lot is full"
+                           "Un-parked car \"KA-123\" at slot 1"
+                           "Parked in slot number 1"]]
       (runner/run (util/test-command-reader input-commands) (util/test-writer output-capture-list))
       (is (= expected-output @output-capture-list)))))
