7d33300bcfb21113d2d2ca16b85f39dcc838952f
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2021-09-27 00:13:14 +0530

    Simplify executor tests.
    This removes the mocks in park and leave and makes the tests simpler to understand.

--- test/amazing_parking_lot/command/executor_test.clj
+++ test/amazing_parking_lot/command/executor_test.clj
@@ -14,57 +14,50 @@
             expected-parking-lot {:number-of-slots 6, :slots [nil nil nil nil nil nil]}]
         (is (= (message resulting-option) "Created parking lot with 6 slots"))
         (is (= (parking-lot resulting-option) expected-parking-lot)))))
-
-  (testing "when the command is park a car"
-    (testing "it creates a parked message and sets the updated parking lot"
-      (with-redefs [parking-lot/park (fn [_ _]
-                                       (event/create-car-parked-event :park-car
-                                                                      1
-                                                                      (car/create "ABC" "White")
-                                                                      :expected-parking-lot))]
-        (let [park-command (create-valid-option "park" ["ABC" "White"])
-              resulting-option (execute park-command)]
-          (is (= "Parked in slot number 1" (message resulting-option)))
-          (is (= :expected-parking-lot (parking-lot resulting-option)))))))
-  (testing "when the command is leave a car"
-    (testing "it executes the command, sets the message and returns the result"
-      (let [actual-slot-number (atom nil)
-            actual-parking-lot (atom nil)]
-        (with-redefs [parking-lot/leave (fn [received-slot-number received-parking-lot]
-                                          (reset! actual-slot-number received-slot-number)
-                                          (reset! actual-parking-lot received-parking-lot)
-                                          (event/create-car-un-parked-event 1 (car/create "ABC" "White")
-                                                                            :expected-parking-lot))]
-          (let [leave-command (assoc (create-valid-option "leave" ["1"]) :parking-lot :expected-parking-lot)
-                resulting-option (execute leave-command)]
-            (is (= "Un-parked car ABC at slot 1" (message resulting-option)))
-            (is (= (parking-lot resulting-option) :expected-parking-lot))
-            (is (= "1" @actual-slot-number))
-            (is (= :expected-parking-lot @actual-parking-lot)))))))
-  (testing "when the command is a query command"
-    (let [car-one (car/create "ABC" "white")
-          car-two (car/create "PQR" "red")
-          car-three (car/create "XYZ" "white")
-          parking-lot {:number-of-slots 3
-                       :slots           [car-one car-two car-three]}]
-      (testing "when the command is registration_numbers_for_cars_with_colour"
-        (testing "it returns the list of registration numbers of cars with specified color"
-          (let [registration-numbers-for-cars-with-colour (assoc (create-valid-option "registration_numbers_for_cars_with_colour" ["white"])
-                                                            :parking-lot parking-lot)
-                resulting-option (execute registration-numbers-for-cars-with-colour)]
-            (is (= "ABC, XYZ" (message resulting-option)))
-            (is (= (:parking-lot resulting-option) parking-lot)))))
-      (testing "when the command is slot_numbers_for_cars_with_colour"
-        (testing "it returns the list of slot numbers of cars with specified color"
-          (let [slot-numbers-for-cars-with-colour (assoc (create-valid-option "slot_numbers_for_cars_with_colour" ["white"])
-                                                    :parking-lot parking-lot)
-                resulting-option (execute slot-numbers-for-cars-with-colour)]
-            (is (= "1, 3" (message resulting-option)))
-            (is (= (:parking-lot resulting-option) parking-lot)))))
-      (testing "when the command is slot_number_for_registration_number"
-        (testing "it returns the list of slot number of the car with specified registration number"
-          (let [slot-numbers-for-cars-with-colour (assoc (create-valid-option "slot_number_for_registration_number" ["ABC"])
-                                                    :parking-lot parking-lot)
-                resulting-option (execute slot-numbers-for-cars-with-colour)]
-            (is (= "1" (message resulting-option)))
-            (is (= (:parking-lot resulting-option) parking-lot))))))))
+  (let [car-one (car/create "ABC" "white")
+        car-two (car/create "PQR" "red")
+        car-three (car/create "XYZ" "white")
+        parking-lot {:number-of-slots 4
+                     :slots           [car-one car-two car-three nil]}]
+    (testing "when the command is park a car"
+      (testing "it creates a parked message and sets the updated parking lot"
+        (let [park-command (add-parking-lot (create-valid-option "park" ["FOP" "White"]) parking-lot)
+              resulting-option (execute park-command)
+              expected-parking-lot {:number-of-slots 4,
+                                    :slots           [{:color "white", :license-number "ABC"}
+                                                      {:color "red", :license-number "PQR"}
+                                                      {:color "white", :license-number "XYZ"}
+                                                      {:color "White", :license-number "FOP"}]}]
+          (is (= "Parked in slot number 4" (message resulting-option)))
+          (is (= expected-parking-lot (:parking-lot resulting-option))))))
+    (testing "when the command is leave a car"
+      (testing "it executes the command, sets the message and returns the result"
+        (let [leave-command (add-parking-lot (create-valid-option "leave" ["1"]) parking-lot)
+              resulting-option (execute leave-command)
+              expected-parking-lot {:number-of-slots 4, :slots [nil
+                                                                {:color "red", :license-number "PQR"}
+                                                                {:color "white", :license-number "XYZ"}
+                                                                nil]}]
+          (is (= "Un-parked car ABC at slot 1" (message resulting-option)))
+          (is (= expected-parking-lot (:parking-lot resulting-option))))))
+    (testing "when the command is registration_numbers_for_cars_with_colour"
+      (testing "it returns the list of registration numbers of cars with specified color"
+        (let [registration-numbers-for-cars-with-colour (assoc (create-valid-option "registration_numbers_for_cars_with_colour" ["white"])
+                                                          :parking-lot parking-lot)
+              resulting-option (execute registration-numbers-for-cars-with-colour)]
+          (is (= "ABC, XYZ" (message resulting-option)))
+          (is (= parking-lot (:parking-lot resulting-option))))))
+    (testing "when the command is slot_numbers_for_cars_with_colour"
+      (testing "it returns the list of slot numbers of cars with specified color"
+        (let [slot-numbers-for-cars-with-colour (assoc (create-valid-option "slot_numbers_for_cars_with_colour" ["white"])
+                                                  :parking-lot parking-lot)
+              resulting-option (execute slot-numbers-for-cars-with-colour)]
+          (is (= "1, 3" (message resulting-option)))
+          (is (= parking-lot (:parking-lot resulting-option))))))
+    (testing "when the command is slot_number_for_registration_number"
+      (testing "it returns the list of slot number of the car with specified registration number"
+        (let [slot-numbers-for-cars-with-colour (assoc (create-valid-option "slot_number_for_registration_number" ["ABC"])
+                                                  :parking-lot parking-lot)
+              resulting-option (execute slot-numbers-for-cars-with-colour)]
+          (is (= "1" (message resulting-option)))
+          (is (= parking-lot (:parking-lot resulting-option))))))))
